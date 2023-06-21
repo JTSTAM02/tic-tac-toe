827d16e29495050b16d90afe8afec6b744395ef6
Tyler Stamm <tyler.stamm1313@gmail.com>
2023-06-21 17:33:19 +0000

    Getting started! Updating game board

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
+const Game = {
+    tiles: Array(9).fill(""),
+    playerA: "x",
+    playerB: "o",
+}
 
+function createGameBoard() {
+    const app = document.getElementById("app");
+    const heading = document.createElement("h1");
+    heading.textContent = "Tic-Tac-Toe";
+    app.appendChild(heading);
+
+    const gameBoard = document.createElement("div");
+    gameBoard.appendChild(tile);
+
+    for(let i = 0; i < 9; i ++) {
+        const tile = document.createElement("div");
+        tile.className = "tile";
+        tile.addEventListener("click", () => handleTileClick(i));
+        gameBoard.appendChild(tile)
+    }
+
+    const restart = docment.createElement("button");
+    restart.textContent = "Restart Game";
+    restart.addEventListener("click", restart);
+    app.appendChild(restart);
+}
+window.addEventListener("load", createGameBoard);
