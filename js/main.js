a78d6c3afe903133241f12360a91f56a82646c8a
Tyler Stamm <tyler.stamm1313@gmail.com>
2023-06-22 14:12:47 +0000

    DisplaySymbols function up and running

--- js/main.js
+++ js/main.js
@@ -1,50 +1,79 @@
-document.addEventListener('DOMContentLoaded', function() {
-      const app = document.getElementById('app');
+document.addEventListener('DOMContentLoaded', function () {
+  const app = document.getElementById('app');
+  let currentPlayer = "X";
 
-      let moves = 0;
-      
-      function createHeading() {
-        const app = document.getElementById("app");
-        const heading = document.createElement("h1");
-        heading.textContent = "Tic-Tac-Toe";
-        app.appendChild(heading);
+  function createHeading() {
+    const app = document.getElementById("app");
+    const heading = document.createElement("h1");
+    heading.textContent = "Tic-Tac-Toe";
+    app.appendChild(heading);
+    const beginGame = document.createElement("button");
+    beginGame.textContent = "Click Here to Begin";
+    beginGame.addEventListener("click", displayBoard);
+    app.appendChild(beginGame);
+  }
+  createHeading();
+
+// creates board with rows and columns then creates tiles and displays them
+function displayBoard() {  
+const board = document.createElement("div");
+  board.classList.add("board");
+  for (let row = 0; row < 3; row++) {
+    for (let col = 0; col < 3; col++) {
+      board.appendChild(createTile(row, col));
     }
+  }
+  function createTile(row, col) {
+    const tile = document.createElement('div');
+    tile.classList.add('tile');
+    return tile;
+  }
+  app.appendChild(board);
 
-    createHeading();
+  // displays restart button after display of game
+  const restartButton = document.createElement('button');
+  restartButton.textContent = 'Restart Game';
+  restartButton.addEventListener('click', restartButton);
+  app.appendChild(restartButton);
 
-      function checkForWin() {
-        const winningCombos = [
-          [0, 1, 2],
-          [3, 4, 5],
-          [6, 7, 8],
-          [0, 3, 6],
-          [1, 4, 7],
-          [2, 5, 8],
-          [0, 4, 8],
-          [2, 4, 6]
-        ];
+  displaySymbols();
+}
 
+  function checkForWin() {
+    const winningCombos = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+  }
+
+function displaySymbols() {
+  const tiles = document.getElementsByClassName("tile");
+  for( let i = 0; i< tiles.length; i ++) {
+    tiles[i].addEventListener("click", function () {
+      if(this.textContent === "") {
+        this.textContent = currentPlayer;
+        currentPlayer = currentPlayer === "X" ? "O" : "X";
       }
+    })
+  }
+}
 
-      function createTile(row, col) {
-        const tile = document.createElement('div');
-        tile.classList.add('tile');
-        return tile;
-      }
 
-      const board = document.createElement("div");
-      board.classList.add("board");
-      for (let row = 0; row < 3; row ++) {
-        for (let col = 0; col < 3; col ++) {
-        board.appendChild(createTile(row, col));
-      }
-    }
+function restartGame() {
+  const tiles = document.getElementsByClassName('tile');
+  for (let i = 0; i < tiles.length; i++) {
+    tiles[i].textContent = '';
+  }
+  currentPlayer = 'X';
+}
 
-        app.appendChild(board);
 
-      const restartButton = document.createElement('button');
-      restartButton.textContent = 'Restart Game';
-      restartButton.addEventListener('click', restartButton);
-      app.appendChild(restartButton);
-    });
+
+});
 
